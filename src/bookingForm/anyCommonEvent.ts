36c70f55376df6642de1b883c151f9393a6ec1e9
Kotelnikov Dmitry <kotelnikoff.dmitry@gmail.com>
2023-11-28 15:38:33 +0300

    Smth

--- src/bookingForm/anyCommonEvent.ts
+++ src/bookingForm/anyCommonEvent.ts
@@ -1,1 +1,9 @@
-console.log('anyCommonEvent')
+import createAnalyticsFunction from '@/createAnalyticsFunction'
+
+type Data = {
+  uuid: string;
+  name: string;
+  id: number;
+}
+
+export default createAnalyticsFunction<Data>('any/common/event')
