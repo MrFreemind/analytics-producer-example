a6b6b1e782e1445eb60b7f1fa0d1684b4940a48b
Kotelnikov Dmitry <kotelnikoff.dmitry@gmail.com>
2023-11-28 15:43:32 +0300

    Fake events

--- src/bookingForm/AEXP/onSubmitButtonClick.ts
+++ src/bookingForm/AEXP/onSubmitButtonClick.ts
@@ -1,1 +1,7 @@
-console.log('onSubmitButtonClick')
+import createAnalyticsFunction from '@/createAnalyticsFunction'
+
+type Data = {
+    source: 'bookingForm' | 'bookingFormModal' | 'bookingFormModalButton';
+}
+
+export default createAnalyticsFunction<Data>('onSubmit/button/click')
